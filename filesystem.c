be573ea7a751dbbaf9a84eb7aae9399031824d0a
daparo <danielapaniaguar@gmail.com>
2021-06-21 20:07:17 -0600

    Cambio

--- filesystem.c
+++ filesystem.c
@@ -1,14 +1,3 @@
-
-/**
-* Less Simple, Yet Stupid Filesystem.
-*
-* Mohammed Q. Hussain - http://www.maastaar.net
-*
-* This is an example of using FUSE to build a simple filesystem. It is a part of a tutorial in MQH Blog with the title "Writing Less Simple, Yet Stupid Filesystem Using FUSE in C": http://maastaar.net/fuse/linux/filesystem/c/2019/09/28/writing-less-simple-yet-stupid-filesystem-using-FUSE-in-C/
-*
-* License: GNU GPL
-*/
-
 #define FUSE_USE_VERSION 30
 
 #include <fuse.h>
